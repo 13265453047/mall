2baacab6e212e27e42fd2e700092f2bba5212dec
bruce <20827012@163.com>
2021-08-04 13:55:54 +0800

    添加消息中间件工程

---
+++ mq/rabbitmq/springboot-rabbitmq/springboot-rabbitmq-provider/src/test/java/com/bruce/mq/SpringbootRabbitmqProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruce.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
