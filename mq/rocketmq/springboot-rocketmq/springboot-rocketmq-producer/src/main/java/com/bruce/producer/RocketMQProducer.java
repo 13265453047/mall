d1c64aca78d9506f55a6065a6cb147a2921a1670
renchongyong <20827012@163.com>
2022-05-13 17:04:56 +0800

    添加springboot-RocketMQ

--- mq/rocketmq/springboot-rocketmq/springboot-rocketmq-producer/src/main/java/com/bruce/producer/RocketMQProducer.java
+++ mq/rocketmq/springboot-rocketmq/springboot-rocketmq-producer/src/main/java/com/bruce/producer/RocketMQProducer.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
  * @className: RocketMQProducer
  * @date 2022-05-12
  */
+
 /**
  * 使用RocketMQ发送三种类型的消息：同步消息、异步消息和单向消息。其中前两种消息是可靠的，因为会有发送是否成功的应答。
  * 使用RocketMQ来消费接收到的消息。
@@ -28,6 +29,15 @@ public class RocketMQProducer {
 //    @Value("${rocketmq.producer.send-message-timeout}")
 //    private Integer messageTimeOut;
 
+
+    /**
+     * 发送普通消息
+     */
+    public void send(String msgBody) {
+        //send spring message
+        rocketMQTemplate.send("queue_test_topic", MessageBuilder.withPayload(msgBody).build());
+    }
+
     /**
      * 发送普通消息
      */
@@ -36,9 +46,20 @@ public class RocketMQProducer {
     }
 
     /**
+     * 发送同步消息
+     *
+     * @param msgBody
+     */
+    public void sendConvertAndSend(String msgBody) {
+        //send message synchronously
+        rocketMQTemplate.convertAndSend("test-queue_test_topic-1", MessageBuilder.withPayload(msgBody).build());
+    }
+
+    /**
      * 发送异步消息 在SendCallback中可处理相关成功失败时的逻辑
      */
     public void sendAsyncMsg(String msgBody) {
+        //send messgae asynchronously
         rocketMQTemplate.asyncSend("queue_test_topic", MessageBuilder.withPayload(msgBody).build(), new SendCallback() {
             @Override
             public void onSuccess(SendResult sendResult) {
@@ -56,7 +77,7 @@ public class RocketMQProducer {
 
     /**
      * 发送延时消息<br/>
-     * 在start版本中 延时消息一共分为18个等级分别为：1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h<br/>
+     * 在start版本中 延时消息一共分为18个等级分别表示的延迟时间为：1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h<br/>
      */
     public void sendDelayMsg(String msgBody, Integer delayLevel) {
         int messageTimeOut = 3000;
@@ -69,5 +90,16 @@ public class RocketMQProducer {
     public void sendTagMsg(String msgBody) {
         rocketMQTemplate.syncSend("queue_test_topic:tag1", MessageBuilder.withPayload(msgBody).build());
     }
+
+    /**
+     * 发送顺序消息
+     *
+     * @param msgBody
+     */
+    public void syncSendOrderly(String msgBody) {
+        //Send messages orderly
+        rocketMQTemplate.syncSendOrderly("orderly_topic", MessageBuilder.withPayload(msgBody).build(), "orderId");
+    }
+
 
 }
