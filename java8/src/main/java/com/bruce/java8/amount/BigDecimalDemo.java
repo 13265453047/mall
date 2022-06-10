7d935811529a6be11787d1366d9e2b45ff3bb3b2
renchongyong <20827012@163.com>
2022-06-10 14:30:09 +0800

    BigDecimal使用精度问题

--- java8/src/main/java/com/bruce/java8/amount/BigDecimalDemo.java
+++ java8/src/main/java/com/bruce/java8/amount/BigDecimalDemo.java
@@ -15,18 +15,20 @@ public class BigDecimalDemo {
         BigDecimal b2 = new BigDecimal(8.8d);
         BigDecimal b3 = new BigDecimal(String.valueOf(8.8d));
         BigDecimal b4 = new BigDecimal("8.8");
+        BigDecimal b5 = new BigDecimal(8.8d).setScale(2, BigDecimal.ROUND_HALF_UP);
         System.out.println(b1);
         System.out.println(b2);
         System.out.println(b3);
         System.out.println(b4);
+        System.out.println(b5);
         /*
         88
         8.800000000000000710542735760100185871124267578125
         8.8
+        8.8
+        8.80
         针对b2这种情况会丢失精度，应使用字符串的形式
          */
-
-
     }
 
 }
