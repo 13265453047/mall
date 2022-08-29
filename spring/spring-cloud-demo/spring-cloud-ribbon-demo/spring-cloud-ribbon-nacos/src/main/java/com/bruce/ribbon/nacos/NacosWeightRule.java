784c5e826a3c15540ab544ef3fe3c2387d2e8807
renchongyong <20827012@163.com>
2022-08-29 12:40:17 +0800

    添加Ribbon项目

---
+++ spring/spring-cloud-demo/spring-cloud-ribbon-demo/spring-cloud-ribbon-nacos/src/main/java/com/bruce/ribbon/nacos/NacosWeightRule.java
@@ -0,0 +1,49 @@
+package com.bruce.ribbon.nacos;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.BaseLoadBalancer;
+import com.netflix.loadbalancer.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author rcy
+ * @version 1.1.0
+ * @className: NacosWeightRule
+ * @date 2022-08-29
+ */
+/**
+ * @ClassName NacosWeightRule
+ * @Description nacos权重规则实现
+ */
+public class NacosWeightRule extends AbstractLoadBalancerRule {
+
+    @Autowired
+    private NacosDiscoveryProperties nacosDiscoveryProperties;
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig iClientConfig) {
+
+    }
+
+    @Override
+    public Server choose(Object o) {
+
+        BaseLoadBalancer loadBalancer = (BaseLoadBalancer) this.getLoadBalancer();
+
+        // 获取将要请求的服务的名称
+        String name = loadBalancer.getName();
+
+        // 拿到服务发现的相关api
+        NamingService namingService = nacosDiscoveryProperties.namingServiceInstance();
+
+        try {
+            // nacos client自动通过基于权重的负载均衡算法，选择一个实例
+            Instance instance = namingService.selectOneHealthyInstance(name);
+
+            return new NacosServer(instance);
+        } catch (NacosException e) {
+            return null;
+        }
+    }
+}
