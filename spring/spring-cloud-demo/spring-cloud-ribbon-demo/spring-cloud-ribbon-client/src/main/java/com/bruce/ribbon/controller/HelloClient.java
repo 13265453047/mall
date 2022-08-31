fc3da004c0c726c6f6e2a1bff070f5fecfd2f03c
renchongyong <20827012@163.com>
2022-08-31 18:03:01 +0800

    添加RibbonClient注解

--- spring/spring-cloud-demo/spring-cloud-ribbon-demo/spring-cloud-ribbon-client/src/main/java/com/bruce/ribbon/controller/HelloClient.java
+++ spring/spring-cloud-demo/spring-cloud-ribbon-demo/spring-cloud-ribbon-client/src/main/java/com/bruce/ribbon/controller/HelloClient.java
@@ -1,33 +1,41 @@
-//package com.bruce.ribbon.controller;
-//
-//import com.bruce.ribbon.RibbonConfiguration;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.cloud.netflix.ribbon.RibbonClient;
-//import org.springframework.cloud.netflix.ribbon.RibbonClients;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.client.RestTemplate;
-//
-///**
-// * @author rcy
-// * @version 1.1.0
-// * @className: HelloClient
-// * @date 2022-08-29
-// */
-//@RestController
-////@RibbonClients(
-////        {@RibbonClient(name = "user-server", configuration = RibbonConfiguration.class)}
-////)
-//@RibbonClient(name = "user-server", configuration = RibbonConfiguration.class)
-//public class HelloClient {
-//
-//    @Autowired
-//    private RestTemplate restTemplate;
-//
-//    @RequestMapping("/hello/{name}")
-//    public String hello(@PathVariable(name = "name") String name) {
-//        return restTemplate.getForObject("http://user-server/hello/" + name, String.class);
-//    }
-//}
-//
+package com.bruce.ribbon.controller;
+
+import com.bruce.ribbon.RibbonConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author rcy
+ * @version 1.1.0
+ * @className: HelloClient
+ * @date 2022-08-29
+ */
+@RestController
+//@RibbonClients(
+//        {@RibbonClient(name = "user-server", configuration = RibbonConfiguration.class)}
+//)
+@RibbonClient(name = "spring-cloud-ribbon-user", configuration = RibbonConfiguration.class)
+public class HelloClient {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable(name = "name") String name) {
+        return restTemplate.getForObject("http://spring-cloud-ribbon-user/hello/" + name, String.class);
+    }
+}
+
--- spring/spring-cloud-demo/spring-cloud-ribbon-demo/spring-cloud-ribbon-client/src/main/java/com/bruce/ribbon/controller/HelloLoadBalancerAnnotation.java
+++ spring/spring-cloud-demo/spring-cloud-ribbon-demo/spring-cloud-ribbon-client/src/main/java/com/bruce/ribbon/controller/HelloLoadBalancerAnnotation.java
@@ -1,35 +1,35 @@
-package com.bruce.ribbon.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-/**
- * @author rcy
- * @version 1.1.0
- * @className: HelloClient
- * @date 2022-08-29
- */
-@RestController
-public class HelloLoadBalancerAnnotation {
-
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @RequestMapping("/hello/{name}")
-    public String hello(@PathVariable(name = "name") String name) {
-        return restTemplate.getForObject("http://spring-cloud-ribbon-user/hello/" + name, String.class);
-    }
-}
-
+//package com.bruce.ribbon.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.web.client.RestTemplateBuilder;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.RestTemplate;
+//
+///**
+// * @author rcy
+// * @version 1.1.0
+// * @className: HelloClient
+// * @date 2022-08-29
+// */
+//@RestController
+//public class HelloLoadBalancerAnnotation {
+//
+//    @LoadBalanced
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    @RequestMapping("/hello/{name}")
+//    public String hello(@PathVariable(name = "name") String name) {
+//        return restTemplate.getForObject("http://spring-cloud-ribbon-user/hello/" + name, String.class);
+//    }
+//}
+//
