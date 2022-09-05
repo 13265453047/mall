7135c0b3ac1f03aa5c650813cc9e2519bed4f9ff
13265453047 <20827012@163.com>
2022-09-05 21:06:48 +0800

    添加Hystrix模块

---
+++ spring/spring-cloud-demo/spring-cloud-hystrix-demo/spring-cloud-hystrix-service/src/main/java/com/bruce/hystrix/SpringCloudHystrixServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bruce.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHystrixServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixServiceApplication.class, args);
+    }
+
+}
